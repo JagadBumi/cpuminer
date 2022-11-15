fdf3ab463b043da17e29542b18b93dce518c4fbe
JagadBumi <66741826+JagadBumi@users.noreply.github.com>
2022-11-16 02:41:36 +0700

    Add files via upload

---
+++ minerd.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git clone https://github.com/crypto-jeronimo/cpuminer-yescrypt-r16 minerd
+cd minerd
+sudo apt-get install build-essential libcurl4-openssl-dev autotools-dev automake
+./autogen.sh
+./nomacro.pl
+./configure CFLAGS="-O3"
+make
