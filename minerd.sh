ecfa737098f35590aed1b8c67ec9835d8562b04a
JagadBumi <66741826+JagadBumi@users.noreply.github.com>
2022-11-16 23:10:24 +0700

    Update minerd.sh

--- minerd.sh
+++ minerd.sh
@@ -7,20 +7,15 @@ chmod +x *
 sudo apt-get install build-essential libcurl4-openssl-dev autotools-dev automake -y
 clear
 ./autogen.sh
-clear
 ./nomacro.pl
-clear
 ./configure CFLAGS="-O3"
-clear
-echo "Make minerd..."
 make
 clear
 mv minerd /bin/
 sudo apt-get autoremove build-essential libcurl4-openssl-dev autotools-dev automake -y | echo "Removing cache.."
-clear
 cd ~
 rm -rf minerd
 rm -rf .cache
 echo "Type command:\nminerd\nminerd --help\nminerd -o startum+tcp://host:port -u walletAddress -p x\nVisit https://zpool.ca for mining.."
-sleep 2
+sleep 5
 clear
