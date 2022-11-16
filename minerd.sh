e865d179954f8f853cb78a2f3eef6ad3b857a675
JagadBumi <66741826+JagadBumi@users.noreply.github.com>
2022-11-16 22:22:41 +0700

    Update minerd.sh

--- minerd.sh
+++ minerd.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
+cd ~
 git clone https://github.com/crypto-jeronimo/cpuminer-yescrypt-r16 minerd
+clear
 cd minerd
 chmod +x *
 sudo apt-get install build-essential libcurl4-openssl-dev autotools-dev automake
+clear
 ./autogen.sh
+clear
 ./nomacro.pl
+clear
 ./configure CFLAGS="-O3"
+clear
 make
+clear
+mv minerd /bin/
+cd ~
+rm -rf minerd
+rm -rf .cache
+echo "Type command:\nminerd\nminerd --help\nminerd -o startum+tcp://host:port -u walletAddress -p x\nVisit https://zpool.ca for mining.."
+clear
