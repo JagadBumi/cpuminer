c40c53d4d01723544f02cac149dbf9c8a9d12502
JagadBumi <66741826+JagadBumi@users.noreply.github.com>
2022-11-19 19:28:13 +0700

    Update minerd.sh

--- minerd.sh
+++ minerd.sh
@@ -4,7 +4,7 @@ git clone https://github.com/crypto-jeronimo/cpuminer-yescrypt-r16 minerd
 cd minerd
 chmod +x *
 apt-get update
-apt-get update
+apt-get upgrade
 apt-get install build-essential libcurl4-openssl-dev autotools-dev automake -y
 ./autogen.sh
 ./nomacro.pl
