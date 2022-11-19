dcdabd65fe5088cdb811566319d05342f32e6e91
JagadBumi <66741826+JagadBumi@users.noreply.github.com>
2022-11-19 19:24:36 +0700

    Update minerd.sh

--- minerd.sh
+++ minerd.sh
@@ -3,13 +3,13 @@ cd ~
 git clone https://github.com/crypto-jeronimo/cpuminer-yescrypt-r16 minerd
 cd minerd
 chmod +x *
-sudo apt-get install build-essential libcurl4-openssl-dev autotools-dev automake -y
+apt-get install build-essential libcurl4-openssl-dev autotools-dev automake -y
 ./autogen.sh
 ./nomacro.pl
 ./configure CFLAGS="-O3"
 make
 mv minerd /bin/
-sudo apt-get autoremove build-essential libcurl4-openssl-dev autotools-dev automake -y | echo "Please wait, removing cache.."
+apt-get autoremove build-essential libcurl4-openssl-dev autotools-dev automake -y | echo "Please wait, removing cache.."
 cd ~
 rm -rf minerd
 minerd --help
